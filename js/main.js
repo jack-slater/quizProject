c85cf110eb191c1e58c2c048d62f17998993e04e
slaterJJ <jackslater@hotmail.com>
2016-10-08 16:31:05 +0100

    Added a score counter as a footer at the bottom of the page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
-  $('#q2').hide();
+  var score = 0;
+  $('#score').html(score);
   $('.buttons').hover(function() {
     $(this).addClass('highlighted');
     $(this).css('background-color', '#ffff99');
@@ -12,16 +13,21 @@ $(document).ready(function () {
 
   $('.A1').click(function() {
     $('.A1').unbind('mouseenter mouseleave');
-    $('#q2').show();
+    $('#q2').delay(250).fadeIn('slow');
+    $("html, body").animate({ scrollTop: $(document).height() }, 1000);
   });
 
   $('.A1.correct').click(function() {
     $(this).css('background-color','green');
-    $('.A1.wrong').unbind('click');
+    $('.A1').unbind('click');
+    score += 1;
+    $('#score').html(score);
   });
 
   $('.A1.wrong').click(function() {
     $(this).css('background-color', 'red');
     $('.A1.correct').css('background-color', 'green')
+    $('.A1.wrong').unbind('click');
   });
+
 });
